80e1ea4790cfc6763b3d81dc17586ce657402a92
Abelardo-Carvalho <abelardocarvalhoneto@gmail.com>
2024-01-18 14:12:37 -0300

    frist commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno();
+        a1.setNome("Cláudio");
+        a1.setMatricula(1111);
+        a1.setCurso("Informática");
+        a1.setSexo("M");
+        a1.pagarMensalidade();
+
+        Bolsista b1 = new Bolsista();
+        
+
+    }
+}
